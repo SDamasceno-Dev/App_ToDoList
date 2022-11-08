1d7b0eb5c1cc1b03e936ab5e49166e978ae8505e
SDamasceno-Dev <sdamasceno.dev@gmail.com>
2022-11-08 14:12:56 -0300

    Component InputAssignment created

---
+++ src/components/InputAssignment/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 54,
+    padding: 16,
+    borderRadius: 6,
+    backgroundColor: '#262626',
+    color: '#F2F2F2',
+    fontSize: 16,
+  }
+});
